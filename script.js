0fb3d74f843df7f16b6f3d26b03d7c751986b545
saroj <sarojpradhan7655@gmail.com>
2023-11-07 18:21:04 +0530

    making the whole project

---
+++ script.js
@@ -0,0 +1,20 @@
+const box1 = document.getElementById('box1');
+box1.addEventListener('click', function(){
+   const body = document.querySelector('body');
+   body.style.backgroundColor = 'aqua'; 
+})
+const box2 = document.getElementById('box2');
+box2.addEventListener('click', function(){
+   const body = document.querySelector('body');
+   body.style.backgroundColor = 'red'; 
+})
+const box3 = document.getElementById('box3');
+box3.addEventListener('click', function(){
+   const body = document.querySelector('body');
+   body.style.backgroundColor = 'greenyellow'; 
+})
+const box4 = document.getElementById('box4');
+box4.addEventListener('click', function(){
+   const body = document.querySelector('body');
+   body.style.backgroundColor = 'blue'; 
+})
